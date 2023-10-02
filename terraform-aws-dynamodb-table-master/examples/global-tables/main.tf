aeae02ce89f9da43c51af2669e154df4a3e27cc1
Sid <siddharth.sidh007@gmail.com>
2023-10-02 18:11:43 +0530

    first commit

---
+++ terraform-aws-dynamodb-table-master/examples/global-tables/main.tf
@@ -0,0 +1,106 @@
+# provider "aws" {
+#   region = "eu-west-1"
+# }
+
+# provider "aws" {
+#   alias  = "euwest2"
+#   region = "eu-west-2"
+# }
+
+locals {
+  tags = {
+    Terraform   = "true"
+    Environment = "staging"
+  }
+}
+
+################################################################################
+# Supporting Resources
+################################################################################
+
+resource "random_pet" "this" {
+  length = 2
+}
+
+# resource "aws_kms_key" "primary" {
+#   description = "CMK for primary region"
+#   tags        = local.tags
+# }
+
+# resource "aws_kms_key" "secondary" {
+#   provider = aws.euwest2
+
+#   description = "CMK for secondary region"
+#   tags        = local.tags
+# }
+
+################################################################################
+# DynamoDB Global Table
+################################################################################
+
+module "dynamodb_table" {
+  source = "../../"
+
+  name             = "cms-cloudeq-${random_pet.this.id}"
+  billing_mode     = var.billing_mode
+  # read_capacity    = var.read_capacity
+  # write_capacity   = var.write_capacity
+  hash_key         = "id"
+  range_key        = "title"
+  stream_enabled   = true
+  stream_view_type = "NEW_AND_OLD_IMAGES"
+
+  server_side_encryption_enabled     = true
+  //server_side_encryption_kms_key_arn = aws_kms_key.primary.arn
+
+  attributes = [
+    {
+      name = "id"
+      type = "N"
+    },
+    {
+      name = "title"
+      type = "S"
+    },
+    {
+      name = "age"
+      type = "N"
+    }
+  ]
+
+  global_secondary_indexes = [
+    {
+      name               = "GSI1"
+      hash_key           = "title"
+      range_key          = "age"
+      projection_type    = "ALL"
+      #non_key_attributes = ["id"]
+    },
+
+    {
+      name               = "GSI2"
+      hash_key           = "id"
+      range_key          = "age"
+      projection_type    = "ALL"
+      #non_key_attributes = ["title"]
+    },
+
+    {
+      name               = "GSI3"
+      hash_key           = "title"
+      range_key          = "id"
+      projection_type    = "ALL"
+      # non_key_attributes = ["age"]
+    }
+
+  ]
+
+  # replica_regions = [{
+  #   region_name            = "eu-west-2"
+  #   kms_key_arn            = aws_kms_key.secondary.arn
+  #   propagate_tags         = true
+  #   point_in_time_recovery = true
+  # }]
+
+  tags = local.tags
+}
---
+++ terraform-aws-dynamodb-table-master/examples/global-tables/terraform.tfvars
@@ -0,0 +1,4 @@
+billing_mode = "PAY_PER_REQUEST"
+read_capacity = 10
+write_capacity = 5
+
